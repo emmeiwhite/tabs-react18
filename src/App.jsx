fff90491d01e623c3ddd70b917283159bec95e45
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-11 16:01:48 +0530

    Created Loader with Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import Loader from './components/Loader'
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Loader />
+    </div>
+  )
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export default function Loader() {
+  return <Wrapper></Wrapper>
+}
+
+const Wrapper = styled.article`
+  width: 200px;
+  height: 200px;
+  border-radius: 100%;
+  border: 10px solid #ccc;
+  border-top-color: #007bff;
+  border-bottom-color: #17a2b8;
+  animation: rotate 0.5s linear infinite;
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`
